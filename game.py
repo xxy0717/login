db7364ea97c4852b174cc3b4b23b7efd3b6c336b
xxy0717 <125196979+xxy0717@users.noreply.github.com>
2023-02-23 17:12:15 +0800

    Update game.py

--- game.py
+++ game.py
@@ -1,45 +1,71 @@
+import streamlit as st
 import numpy as np
-import torch
-from PIL import Image
-from io import BytesIO
 
 class Game:
-    MACHINE = 1
-    USER = -1
+    BLACK = -1
+    WHITE = 1
+    BOARD_SIZE = 15
+    PIECE_RADIUS = 20
 
-    def __init__(self):
-        self.board_size = 19
-        self.board = np.zeros((self.board_size, self.board_size), dtype=np.int32)
-        self.started = False
-        self.turn = None
-        self.winner = None
-        self.machine_difficulty = 5
-        self.model = None
+    def __init__(self, mode, ai_level=1):
+        self.mode = mode
+        self.ai_level = ai_level
+        self.reset_game()
 
-    def start_game(self, difficulty):
-        self.started = True
-        self.turn = Game.USER
-        self.winner = None
-        self.machine_difficulty = difficulty
-        self.model = torch.load("model.pt")
+    def reset_game(self):
+        self.turn = self.BLACK
+        self.board = np.zeros((self.BOARD_SIZE, self.BOARD_SIZE), dtype=int)
+        self.history = []
 
-    def draw_board(self):
-        board_img = Image.new("RGB", (512, 512), (255, 206, 158))
-        stone_size = 30
-        for row in range(self.board_size):
-            for col in range(self.board_size):
-                if self.board[row, col] == Game.USER:
-                    color = "black"
-                elif self.board[row, col] == Game.MACHINE:
-                    color = "white"
-                else:
-                    continue
-                x = col * stone_size + stone_size
-                y = row * stone_size + stone_size
-                ImageDraw.Draw(board_img).ellipse((x - stone_size, y - stone_size, x + stone_size, y + stone_size), fill=color)
-        return np.array(board_img)
+    def run(self):
+        canvas_width = self.BOARD_SIZE * self.PIECE_RADIUS * 2
+        canvas_height = self.BOARD_SIZE * self.PIECE_RADIUS * 2
 
-    def check_game_over(self):
-        for row in range(self.board_size):
-            for col in range(self.board_size):
-                if self.board[row
+        canvas = st.canvas(width=canvas_width, height=canvas_height, drawing_mode='transform')
+
+        while True:
+            # Draw board
+            self.draw_board(canvas)
+
+            # Handle user input
+            if self.mode == 'Human vs. AI' and self.turn == self.WHITE:
+                self.make_ai_move()
+            else:
+                clicked = canvas_clicked(canvas)
+                if clicked:
+                    x, y = clicked
+                    row, col = self.get_clicked_position(x, y)
+                    if self.is_valid_move(row, col):
+                        self.make_move(row, col)
+
+            # Check for end of game
+            winner = self.check_for_winner()
+            if winner is not None:
+                self.show_winner(canvas, winner)
+                if st.button('Play again'):
+                    self.reset_game()
+                    break
+
+    def make_move(self, row, col):
+        self.board[row][col] = self.turn
+        self.history.append((row, col, self.turn))
+        self.turn = -self.turn
+
+    def is_valid_move(self, row, col):
+        if row < 0 or row >= self.BOARD_SIZE or col < 0 or col >= self.BOARD_SIZE:
+            return False
+        if self.board[row][col] != 0:
+            return False
+        if self.is_ko_move(row, col):
+            return False
+        return True
+
+    def is_ko_move(self, row, col):
+        if len(self.history) < 2:
+            return False
+        last_move = self.history[-1]
+        if (row, col) != last_move[:2]:
+            return False
+        prev_board = np.zeros((self.BOARD_SIZE, self.BOARD_SIZE), dtype=int)
+        prev_board[self.board == -self.turn] = -self.turn
+        prev
