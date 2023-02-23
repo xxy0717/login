da0e263165270b8796885589fb8539d4b42745c8
xxy0717 <125196979+xxy0717@users.noreply.github.com>
2023-02-23 11:35:38 +0800

    Update login.py

--- login.py
+++ login.py
@@ -1,68 +1,43 @@
 import streamlit as st
-from passlib.hash import pbkdf2_sha256
-import sqlite3
+from game import Game
 
-# 创建数据库
-conn = sqlite3.connect('users.db')
-c = conn.cursor()
-c.execute('''CREATE TABLE IF NOT EXISTS users
-             (id INTEGER PRIMARY KEY AUTOINCREMENT,
-              email TEXT,
-              password TEXT)''')
-conn.commit()
+# Set page title
+st.set_page_config(page_title="围棋游戏", page_icon=":memo:", layout="wide")
 
-# 注册功能
-def register():
-    st.header("用户注册")
-    email = st.text_input("请输入您的Email：")
-    password = st.text_input("请输入密码：", type="password")
-    confirm_password = st.text_input("请确认密码：", type="password")
-    if st.button("注册"):
-        # 验证输入的密码和确认密码是否匹配
-        if password != confirm_password:
-            st.error("两次输入的密码不匹配！")
+# Create a new game
+game = Game()
+
+# Title
+st.title("围棋游戏")
+
+# Sidebar
+st.sidebar.title("游戏设置")
+difficulty = st.sidebar.slider("请选择机器的围棋水平", 1, 10, 5)
+
+# Main content
+if st.button("开始新游戏"):
+    game.start_game(difficulty)
+
+if game.started:
+    # Draw the board
+    st.write("当前棋局：")
+    st.image(game.draw_board(), use_column_width=True)
+
+    # Check if game is over
+    if game.check_game_over():
+        st.write("游戏结束！")
+        if game.winner is None:
+            st.write("平局！")
         else:
-            # 检查是否存在相同的Email
-            query = f"SELECT * FROM users WHERE email='{email}'"
-            result = c.execute(query).fetchone()
-            if result:
-                st.error("该Email已被注册，请使用其他Email！")
-            else:
-                # 对密码进行加密
-                hashed_password = pbkdf2_sha256.hash(password)
-                # 将新用户插入到数据库中
-                query = f"INSERT INTO users (email, password) VALUES ('{email}', '{hashed_password}')"
-                c.execute(query)
-                conn.commit()
-                st.success("注册成功，请登录！")
+            st.write(f"{game.winner} 获胜！")
 
-# 登录功能
-def login():
-    st.header("用户登录")
-    email = st.text_input("请输入您的Email：")
-    password = st.text_input("请输入密码：", type="password")
-    if st.button("登录"):
-        # 检查输入的Email和密码是否与数据库匹配
-        query = f"SELECT * FROM users WHERE email='{email}'"
-        result = c.execute(query).fetchone()
-        if result:
-            if pbkdf2_sha256.verify(password, result[2]):
-                st.success("登录成功！")
-            else:
-                st.error("密码不正确，请重新输入！")
-        else:
-            st.error("该Email未注册，请先注册！")
+    # Check if it's the machine's turn
+    elif game.turn == Game.MACHINE:
+        st.write("机器正在思考中...")
+        game.make_move()
 
-# 显示界面
-def main():
-    st.set_page_config(page_title="用户注册和登录")
-    st.title("用户注册和登录")
-    menu = ["用户注册", "用户登录"]
-    choice = st.sidebar.selectbox("请选择功能：", menu)
-    if choice == "用户注册":
-        register()
+    # Let the user make a move
     else:
-        login()
-
-if __name__ == '__main__':
-    main()
+        col, row = st.number_input("请输入你的落子位置（例如：3,4）", value=(0, 0), step=1)
+        if st.button("下子"):
+            game.make_move((col, row))
