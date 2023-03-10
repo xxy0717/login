3c88c04f29719c4b1d3353ca91bc74c0cabb5072
xxy0717 <125196979+xxy0717@users.noreply.github.com>
2023-03-10 15:15:13 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,15 @@
 import schedule
 import time
 import requests
+import pytz
 
 def job():
-    url = "https://www.imddoctors.com/science/ekx/szjsqf/1280"  # 这里替换为您需要访问的URL
+    url = "https://www.example.com"  # 这里替换为您需要访问的URL
     response = requests.get(url)
     print(response.status_code)  # 打印访问结果
 
-# 每天的8:00执行一次任务
-schedule.every().day.at("7:10").do(job)
+beijing_tz = pytz.timezone("Asia/Shanghai")  # 设置时区为北京时间
+schedule.every().day.at("15:20").do(job).timezone(beijing_tz)  # 在北京时间7:10执行任务
 
 # 定时任务主循环
 while True:
