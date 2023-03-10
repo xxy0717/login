7be92e32829b054e12d3aeaf600ad7b8b2067450
xxy0717 <125196979+xxy0717@users.noreply.github.com>
2023-03-10 15:04:54 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,26 +1,16 @@
-import streamlit as st
-from game import Game
+import schedule
+import time
+import requests
 
-st.set_page_config(page_title='Weiqi Game')
+def job():
+    url = "https://www.imddoctors.com/science/ekx/szjsqf/1280"  # 这里替换为您需要访问的URL
+    response = requests.get(url)
+    print(response.status_code)  # 打印访问结果
 
-# Define game modes
-HUMAN_VS_HUMAN = 'Human vs. Human'
-HUMAN_VS_AI = 'Human vs. AI'
+# 每天的8:00执行一次任务
+schedule.every().day.at("15:08").do(job)
 
-# Define default settings
-DEFAULT_MODE = HUMAN_VS_AI
-DEFAULT_AI_LEVEL = 2
-
-def main():
-    st.title('Weiqi Game')
-    mode = st.sidebar.radio('Select game mode:', (HUMAN_VS_HUMAN, HUMAN_VS_AI), index=1)
-
-    if mode == HUMAN_VS_HUMAN:
-        st.warning('Human vs. Human mode is not yet supported.')
-    else:
-        ai_level = st.sidebar.slider('Select AI level (1-5):', 1, 5, DEFAULT_AI_LEVEL)
-        game = Game(mode=mode, ai_level=ai_level)
-        game.run()
-
-if __name__ == '__main__':
-    main()
+# 定时任务主循环
+while True:
+    schedule.run_pending()
+    time.sleep(1)
